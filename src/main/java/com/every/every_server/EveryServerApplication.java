0569fe1d57bed63e9020e38249927d9d57eb8d82
choi jinwoo <chlwlsdn0828@gmail.com>
2020-01-22 16:02:31 +0900

    [INIT] init

---
+++ src/main/java/com/every/every_server/EveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.every.every_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/every/every_server/EveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.every.every_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
