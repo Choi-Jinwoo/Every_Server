ce228d624174f258a4135dd347e2969e212fd84c
choi jinwoo <chlwlsdn0828@gmail.com>
2020-01-28 17:09:31 +0900

    [DELETE] test code

--- src/main/java/com/every/every_server/constant/DateConstant.java
+++ src/main/java/com/every/every_server/constant/DateConstant.java
@@ -1,8 +1,5 @@
 package com.every.every_server.constant;
 
-import java.time.Year;
-import java.util.Date;
-
 public class DateConstant {
     public static int MILLISECONDS_IN_A_HOUR = 1000 * 60 * 60;
 }
