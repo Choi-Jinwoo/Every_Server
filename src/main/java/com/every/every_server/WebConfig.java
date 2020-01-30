781a1d10362a30af2e0c9644259399615ce7a473
choi jinwoo <chlwlsdn0828@gmail.com>
2020-01-30 11:28:24 +0900

    [ADD] Cors

---
+++ src/main/java/com/every/every_server/WebConfig.java
@@ -0,0 +1,18 @@
+package com.every.every_server;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
